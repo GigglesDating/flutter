97942c900f7983d09abc4597fd7ff3efc0266c76
Krishna Prasad <director@gigglesdating.com>
2025-02-01 14:48:33 +0530

    changes

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -40,7 +40,7 @@ class _SplashScreenState extends State<SplashScreen>
         transitionDuration: const Duration(milliseconds: 800),
         pageBuilder: (context, animation, secondaryAnimation) => FadeTransition(
           opacity: animation,
-          child: const LoginScreen(),
+          child: const ProfileCreation3(),
         ),
       ),
     );

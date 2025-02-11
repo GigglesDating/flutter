45782c5843b7552d131b8c57e6f60f52226b3e08
AbhishekValiyapurakkal <abhiv6160@gmail.com>
2025-02-11 18:47:21 +0530

    edits

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/screens/barrel.dart';
 import 'package:flutter_frontend/screens/member/explore_tab/swipe_filter_page.dart';
+import 'package:flutter_frontend/screens/user/add_to_story_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -40,7 +42,7 @@ class _SplashScreenState extends State<SplashScreen>
         transitionDuration: const Duration(milliseconds: 800),
         pageBuilder: (context, animation, secondaryAnimation) => FadeTransition(
           opacity: animation,
-          child: SwipeFilterPage(),
+          child: AddToStoryScreen(),
         ),
       ),
     );

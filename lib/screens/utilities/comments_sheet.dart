56b135de0218322b785d13e0602288f0ab3c0302
unknown <director@gigglesdating.com>
2025-02-18 14:55:02 +0530

    changes

--- lib/screens/utilities/comments_sheet.dart
+++ lib/screens/utilities/comments_sheet.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
-import 'dart:io' show Platform;
 import 'dart:ui';
 
 class CommentsSheet extends StatefulWidget {
@@ -23,7 +21,6 @@ class CommentsSheet extends StatefulWidget {
 
 class _CommentsSheetState extends State<CommentsSheet> {
   final TextEditingController _commentController = TextEditingController();
-  bool _showEmojis = false;
   String? _replyingTo;
 
   final List<Map<String, dynamic>> _comments = [
@@ -41,75 +38,78 @@ class _CommentsSheetState extends State<CommentsSheet> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
+    final screenSize = mediaQuery.size;
     final bottomPadding = mediaQuery.viewInsets.bottom;
-    final availableHeight = mediaQuery.size.height - bottomPadding;
-    final maxSheetHeight = availableHeight * 0.7;
-    final minSheetHeight = availableHeight * 0.4;
+    final topPadding = mediaQuery.padding.top;
 
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      height: _showEmojis
-          ? maxSheetHeight
-          : bottomPadding > 0
-              ? maxSheetHeight
-              : minSheetHeight,
-      child: ClipRRect(
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-          child: Container(
-            decoration: BoxDecoration(
-              color: widget.isDarkMode
-                  ? Colors.black.withAlpha(240)
-                  : Colors.white.withAlpha(240),
-              borderRadius:
-                  const BorderRadius.vertical(top: Radius.circular(25)),
-              border: Border.all(
+    // Calculate available space
+    final availableHeight = screenSize.height - bottomPadding - topPadding;
+    final maxSheetHeight = availableHeight * 0.8;
+    final minSheetHeight = availableHeight * 0.5;
+
+    return GestureDetector(
+      onTap: () {
+        // Dismiss keyboard when tapping outside input
+        if (_commentController.text.isEmpty) {
+          FocusScope.of(context).unfocus();
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        height: bottomPadding > 0 ? maxSheetHeight * 0.8 : minSheetHeight,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+            child: Container(
+              decoration: BoxDecoration(
                 color: widget.isDarkMode
-                    ? Colors.white.withAlpha(38)
-                    : Colors.black.withAlpha(26),
+                    ? Colors.black.withAlpha(240)
+                    : Colors.white.withAlpha(240),
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(25)),
+                border: Border.all(
+                  color: widget.isDarkMode
+                      ? Colors.white.withAlpha(38)
+                      : Colors.black.withAlpha(26),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withAlpha(51),
+                    blurRadius: 20,
+                    spreadRadius: -5,
+                    offset: const Offset(0, -5),
+                  ),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withAlpha(51),
-                  blurRadius: 20,
-                  spreadRadius: -5,
-                  offset: const Offset(0, -5),
-                ),
-              ],
-            ),
-            child: Column(
-              children: [
-                // Drag Handle
-                Center(
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      top: widget.screenWidth * 0.04,
-                      bottom: widget.screenWidth * 0.02,
-                    ),
-                    width: widget.screenWidth * 0.1,
-                    height: 4,
-                    decoration: BoxDecoration(
-                      color: widget.isDarkMode
-                          ? Colors.white.withAlpha(77)
-                          : Colors.black.withAlpha(77),
-                      borderRadius: BorderRadius.circular(2),
+              child: Column(
+                children: [
+                  // Drag Handle
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        top: widget.screenWidth * 0.04,
+                        bottom: widget.screenWidth * 0.02,
+                      ),
+                      width: widget.screenWidth * 0.1,
+                      height: 4,
+                      decoration: BoxDecoration(
+                        color: widget.isDarkMode
+                            ? Colors.white.withAlpha(77)
+                            : Colors.black.withAlpha(77),
+                        borderRadius: BorderRadius.circular(2),
+                      ),
                     ),
                   ),
-                ),
-                _buildHeader(),
-                Expanded(
-                  child: _buildCommentsList(),
-                ),
-                if (_showEmojis)
-                  SizedBox(
-                    height: widget.screenHeight * 0.35,
-                    child: _buildEmojiPicker(),
+                  _buildHeader(),
+                  Expanded(
+                    child: _buildCommentsList(),
                   ),
-                _buildCommentInput(),
-                // Extra padding for bottom safe area
-                SizedBox(height: mediaQuery.padding.bottom),
-              ],
+                  _buildCommentInput(),
+                  // Extra padding for bottom safe area
+                  SizedBox(height: mediaQuery.padding.bottom),
+                ],
+              ),
             ),
           ),
         ),
@@ -175,28 +175,6 @@ class _CommentsSheetState extends State<CommentsSheet> {
     );
   }
 
-  Widget _buildEmojiPicker() {
-    return EmojiPicker(
-      textEditingController: _commentController,
-      onEmojiSelected: (category, emoji) {
-        setState(() {
-          _commentController.text = _commentController.text + emoji.emoji;
-        });
-      },
-      config: Config(
-        height: 256,
-        checkPlatformCompatibility: true,
-        emojiViewConfig: EmojiViewConfig(
-          emojiSizeMax: 28 * (Platform.isIOS ? 1.2 : 1.0),
-        ),
-        skinToneConfig: const SkinToneConfig(),
-        categoryViewConfig: const CategoryViewConfig(),
-        bottomActionBarConfig: const BottomActionBarConfig(),
-        searchViewConfig: const SearchViewConfig(),
-      ),
-    );
-  }
-
   Widget _buildCommentInput() {
     return Container(
       padding: EdgeInsets.all(widget.screenWidth * 0.04),
@@ -212,45 +190,6 @@ class _CommentsSheetState extends State<CommentsSheet> {
       ),
       child: Row(
         children: [
-          if (_replyingTo != null)
-            Container(
-              margin: EdgeInsets.only(right: widget.screenWidth * 0.02),
-              padding: EdgeInsets.symmetric(
-                horizontal: widget.screenWidth * 0.02,
-                vertical: widget.screenWidth * 0.01,
-              ),
-              decoration: BoxDecoration(
-                color: widget.isDarkMode
-                    ? Colors.white.withAlpha(153)
-                    : Colors.black.withAlpha(153),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Replying to ${_replyingTo}',
-                    style: TextStyle(
-                      fontSize: widget.screenWidth * 0.035,
-                      color: widget.isDarkMode
-                          ? Colors.white.withAlpha(153)
-                          : Colors.black.withAlpha(153),
-                    ),
-                  ),
-                  SizedBox(width: widget.screenWidth * 0.01),
-                  GestureDetector(
-                    onTap: () => setState(() => _replyingTo = null),
-                    child: Icon(
-                      Icons.close,
-                      size: widget.screenWidth * 0.04,
-                      color: widget.isDarkMode
-                          ? Colors.white.withAlpha(153)
-                          : Colors.black.withAlpha(153),
-                    ),
-                  ),
-                ],
-              ),
-            ),
           Expanded(
             child: Container(
               padding: EdgeInsets.symmetric(
@@ -263,61 +202,44 @@ class _CommentsSheetState extends State<CommentsSheet> {
                     : Colors.black.withAlpha(153),
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _commentController,
-                      style: TextStyle(
-                        color: widget.isDarkMode ? Colors.white : Colors.black,
-                      ),
-                      decoration: InputDecoration(
-                        hintText: 'Add a comment...',
-                        hintStyle: TextStyle(
-                          color: widget.isDarkMode
-                              ? Colors.white.withAlpha(153)
-                              : Colors.black.withAlpha(153),
-                        ),
-                        border: InputBorder.none,
-                        isDense: true,
-                        contentPadding: EdgeInsets.zero,
-                      ),
-                    ),
+              child: TextField(
+                controller: _commentController,
+                style: TextStyle(
+                  color: widget.isDarkMode ? Colors.white : Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Add a comment...',
+                  hintStyle: TextStyle(
+                    color: widget.isDarkMode
+                        ? Colors.white.withAlpha(153)
+                        : Colors.black.withAlpha(153),
                   ),
-                  IconButton(
-                    onPressed: () => setState(() => _showEmojis = !_showEmojis),
-                    icon: Icon(
-                      Icons.emoji_emotions_outlined,
-                      color: widget.isDarkMode
-                          ? Colors.white.withAlpha(153)
-                          : Colors.black.withAlpha(153),
-                    ),
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(),
-                  ),
-                  SizedBox(width: widget.screenWidth * 0.02),
-                  IconButton(
-                    onPressed: () {
-                      if (_commentController.text.isNotEmpty) {
-                        // Add comment logic
-                        _commentController.clear();
-                        setState(() => _replyingTo = null);
-                      }
-                    },
-                    icon: Icon(
-                      Icons.send_rounded,
-                      color: _commentController.text.isEmpty
-                          ? (widget.isDarkMode
-                              ? Colors.white.withAlpha(153)
-                              : Colors.black.withAlpha(153))
-                          : Theme.of(context).primaryColor,
-                    ),
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(),
-                  ),
-                ],
+                  border: InputBorder.none,
+                  isDense: true,
+                  contentPadding: EdgeInsets.zero,
+                ),
               ),
             ),
+          ),
+          SizedBox(width: widget.screenWidth * 0.02),
+          IconButton(
+            onPressed: () {
+              if (_commentController.text.isNotEmpty) {
+                // Add comment logic
+                _commentController.clear();
+                setState(() => _replyingTo = null);
+              }
+            },
+            icon: Icon(
+              Icons.send_rounded,
+              color: _commentController.text.isEmpty
+                  ? (widget.isDarkMode
+                      ? Colors.white.withAlpha(153)
+                      : Colors.black.withAlpha(153))
+                  : Theme.of(context).primaryColor,
+            ),
+            padding: EdgeInsets.zero,
+            constraints: const BoxConstraints(),
           ),
         ],
       ),
